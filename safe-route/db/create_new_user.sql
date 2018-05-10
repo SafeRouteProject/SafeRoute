c5c3a849a5fe97ba28014611c4d8ff6aae7ffb64
Joseph Anderson <josephiznot@gmail.com>
2018-05-09 22:17:24 -0500

    user create and authenticated

---
+++ safe-route/db/create_new_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users
+(user_name, user_email, first_name, last_name, phone_number, user_password)
+VALUES ($1, $2, $3, $4, $5, $6)
--- safe-route/server/controllers/authCtrl.js
+++ safe-route/server/controllers/authCtrl.js
@@ -1,20 +1,57 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const getUsers = (req, res) => {
+  req.app
+    .get("db")
+    .get_users()
+    .then(response => {
+      res.status(200).send(response);
+    });
+};
+const createNewUser = (req, res) => {
+  const {
+    userName,
+    email,
+    firstName,
+    lastName,
+    phoneNumber,
+    password
+  } = req.body;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    req.app
+      .get("db")
+      .create_new_user([
+        userName,
+        email,
+        firstName,
+        lastName,
+        phoneNumber,
+        hash
+      ])
+      .then(response => {
+        res.status(200).send("User created!");
+      });
+  });
+};
 const authenticateUser = (req, res) => {
   const { password } = req.body;
   const { id } = req.params;
-  console.log("hit");
   req.app
     .get("db")
     .get_user_password(id)
     .then(response => {
-      response[0].user_password;
+      bcrypt.compare(password, response[0].user_password, (err, hashRes) => {
+        hashRes
+          ? res.status(200).send({ message: "User authenticated" })
+          : res.status(401).send({ message: "Unauthorized" });
+      });
     });
 };
-const createNewUser = (req, res) => {};
 
 module.exports = {
+  createNewUser,
   authenticateUser,
-  createNewUser
+  getUsers
 };
