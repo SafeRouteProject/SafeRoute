364bbcdad74bb27541c99ebb4427c0f8695eae7a
Jacob Anderson <odistiinct@gmail.com>
2018-05-10 08:49:57 -0500

    set up google autocomplete address, found solution to google distances url to postman

--- safe-route/src/store.js
+++ safe-route/src/store.js
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
 import users from "./ducks/user";
 import groups from "./ducks/groups";
+import location from "./ducks/location";
 
 const store = createStore(
-  combineReducers({ users, groups }),
+  combineReducers({ users, groups, location }),
   applyMiddleware(promiseMiddleware())
 );
 
