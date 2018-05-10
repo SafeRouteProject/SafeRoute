b4278a933b52c98cb5970ec6bda1172a81b21a8f
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-10 13:36:33 -0500

    Added To Profile

--- safe-route/src/components/Profile/Profile.js
+++ safe-route/src/components/Profile/Profile.js
@@ -8,8 +8,19 @@ class Profile extends Component {
   }
   render() {
     return (
-      <div>
-        <div classname='profile-pfp' />
+      <div classname='profile-container'>
+        <div className='profile-banner' />
+        <div className='profile-pfp' />
+        <section className='profile-user-info-container'>
+          <h2 className='profile-username'>Username</h2>
+          <h3 className='profile-fullname'>Full Name</h3>
+          <h3 className='profile-email'>useremail@email.com</h3>
+          <h3 className='profile-phone'>555-555-5555</h3>
+        </section>
+
+        <section className='profile-groups-container'>
+          <h3 className='profile-groups'>Your Groups</h3>
+        </section>
       </div>
     );
   }
