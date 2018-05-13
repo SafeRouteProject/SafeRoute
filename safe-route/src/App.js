fba93a195a83a1f852e10064c2054fbf2cc3a298
Jacob Anderson <odistiinct@gmail.com>
2018-05-13 08:28:06 -0500

    adjusted alignment of routes, and moved location from index to App.js

--- safe-route/src/App.js
+++ safe-route/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
+import Profile from "./components/Profile/Profile";
+import LandingLogin from "./components/LandingLogin/LandingLogin";
+import Home from "./components/Home/Home";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Provider store={store}>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <Routes />
+        </MuiThemeProvider>
+      </Provider>
+    );
   }
 }
 
