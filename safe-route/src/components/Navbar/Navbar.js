5527ee48efa10072eb8022064101c9635d94454a
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-13 10:38:52 -0500

    Fixe Router

--- safe-route/src/components/Navbar/Navbar.js
+++ safe-route/src/components/Navbar/Navbar.js
@@ -1,30 +1,31 @@
 import React, { Component } from "react";
 import "./navbar.css";
 
-import { Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
     return (
+      // <div className="navbar-container">
+      //   <div className="navbar-profile-icon">
+      //     <i class="fas fa-exclamation-circle" />
+      //   </div>
+      //   <div className="navbar-profile-icon">
+      //     <i class="fas fa-users" />
+      //   </div>
+      //   <div className="navbar-profile-icon">
+      //     <i class="far fa-user-circle" />
+      //   </div>
       <div className="navbar-container">
-        <div className="navbar-profile-icon">
-          <i class="fas fa-exclamation-circle" />
-        </div>
-        <div className="navbar-profile-icon">
-          <i class="fas fa-users" />
-        </div>
-        <div className="navbar-profile-icon">
-          <i class="far fa-user-circle" />
-        </div>
-        {/* <Link to='/' className="navbar-profile-icon">
+        <Link to="/" className="navbar-profile-icon">
           <i class="fas fa-exclamation-circle" />
         </Link>
-        <Link to='/' className="navbar-profile-icon">
+        <Link to="/login" className="navbar-profile-icon">
           <i class="fas fa-users" />
         </Link>
-        <Link to='/profile' className="navbar-profile-icon">
+        <Link to="/profile" className="navbar-profile-icon">
           <i class="far fa-user-circle" />
-        </Link> */}
+        </Link>
       </div>
     );
   }
