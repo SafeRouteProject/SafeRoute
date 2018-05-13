8609d76f4f3e3cd164d12a13c9d2572706d4a267
Jacob Anderson <odistiinct@gmail.com>
2018-05-13 08:30:32 -0500

    deleted imports

--- safe-route/src/index.js
+++ safe-route/src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import getMuiTheme from "material-ui/styles/getMuiTheme";
-import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 
 ReactDOM.render(<App />, document.getElementById("root"));
