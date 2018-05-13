8327ab65de1050545542c79f026e7e0aa44c9fcc
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-13 14:59:20 -0500

    Fixing Jakes mistake

--- safe-route/src/components/Alert/Alert.js
+++ safe-route/src/components/Alert/Alert.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./alert.css";
 
 import swal from 'sweetalert2';
+// import Button from '@material-ui/core/Button';
 
 class Alert extends Component {
   render() {
     return (
       <div class="alert-container">
         <div className="background" />
+        {/* <Button /> */}
         <div className="alert-btn" onClick={() => swal('Alert Sent!', 'An alert was sent to your dependencies.')}>
           <h1 className="alert-text">Alert</h1>
         </div>
